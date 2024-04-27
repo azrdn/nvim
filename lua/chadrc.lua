ce52d7fd62688a7260ce01d38fd25013c55504e1
Azis Ramadhan <git@azrd.dev>
2024-04-28 00:02:36 +0700

    My config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,8 +5,8 @@
 local M = {}
 
 M.ui = {
-	theme = "onedark",
-
+	theme = "tokyonight",
+  statusline = { theme = "vscode_colored" }
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,22 @@
 require "nvchad.options"
 
 -- add yours here!
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("VimEnter", {
+  callback = function ()
+    vim.cmd(":silent !kitten @ set-spacing padding=0")
+    vim.cmd(":silent !kitten @ disable-ligatures never")
+  end,
+})
+
+autocmd("VimLeavePre", {
+  -- neovim/issues/21856
+  callback = function ()
+    vim.fn.jobstart('kitten @ set-spacing padding=8 margin=0', { detach = true })
+    vim.fn.jobstart('kitten @ disable-ligatures always', { detach = true })
+  end,
+})
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
